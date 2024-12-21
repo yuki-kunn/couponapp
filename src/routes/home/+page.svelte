8591e6e798d962f1f85d6c419609a3d85c35664b
kaja36 <kamowaka4m7@gmail.com>
2024-12-21 22:26:43 +0900

    ホーム画面　タイトル追加

---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,13 @@
+<style>
+.title{
+    font-family: Open Sans;
+    font-size: 64px;
+    font-weight: 700;
+    line-height: 112px;
+    text-align: center;
+    margin-top: 184px;
+    
+}
+</style>
+
+<h1 class="title">未定</h1>
