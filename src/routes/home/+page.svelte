6eb6c8edd7dec2b8fc2962bb73e0eb453efb2d14
kaja36 <kamowaka4m7@gmail.com>
2024-12-23 18:35:10 +0900

    ボタンの配置・スタイルの設定

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,13 +1,40 @@
 <style>
+.home{
+    text-align: center;
+}
 .title{
     font-family: Open Sans;
     font-size: 64px;
     font-weight: 700;
     line-height: 112px;
-    text-align: center;
     margin-top: 184px;
-    
+    margin-bottom: 89px;
 }
+.buttonLayout{
+    display: block;
+    font-family: Inter;
+    font-size: 32px;
+    font-weight: 600;
+    color: white;
+    width: 322px;
+    height: 64px;
+    padding: 10px 16px 10px 16px;
+    margin: 99px auto auto auto;
+    gap: 8px;
+    border-radius: 6px;
+}
+
+.registCoupon{
+    background: #5227CC;
+}
+.summaryCoupon{
+    background: #28A745;
+}
+
 </style>
 
-<h1 class="title">未定</h1>
+<div class="home">
+    <h1 class="title">未定</h1>
+    <a href="../" class="buttonLayout registCoupon">クーポン登録</a>
+    <a href="../" class="buttonLayout summaryCoupon">クーポン一覧</a>
+</div>
