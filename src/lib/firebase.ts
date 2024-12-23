909fa3c50c612659159e45ec86071d3f87e910dd
yuki-kunn <21e1248@andrew.ac.jp>
2024-12-24 04:06:07 +0900

    firebase.ts作成

---
+++ src/lib/firebase.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD8wzIs2Zdrl3Q3gzseAqQkmw-xtKBI1Rw",
+    authDomain: "hackthon20241227.firebaseapp.com",
+    projectId: "hackthon20241227",
+    storageBucket: "hackthon20241227.firebasestorage.app",
+    messagingSenderId: "980992507739",
+    appId: "1:980992507739:web:388de63a10fec30dd6e47e",
+    measurementId: "G-X1XF1NCNET"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
